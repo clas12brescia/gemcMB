d55a1326eabd4be408159b9c9af9283a14644ad9
ggosta <giulia.gosta@unibs.it>
2022-09-01 21:16:07 +0200

    scommented passive Gd

--- nVeto/geometry.pl
+++ nVeto/geometry.pl
@@ -966,7 +966,7 @@ sub make_bdx_CT
     make_main_volume();
     make_nVeto;
     make_lAr;
-  #  make_passive;
+    make_passive;
   #  make_flux_cosmic_sph;		#Crea la routine flux_cosmic che disegna la sfera
 }
 
